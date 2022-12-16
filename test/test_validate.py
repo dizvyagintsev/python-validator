e8980198387fa50c068362dda32c031fbe204315
Denis Zvyagintsev <dizvyagintsev@avito.ru>
2022-12-16 04:13:39 +0300

    logic, tests and ci pipeline

---
+++ test/test_validate.py
@@ -0,0 +1,100 @@
+import pytest
+
+from validator.validate import validate
+
+
+@pytest.mark.parametrize(
+    "code,expected",
+    [
+        (
+            """
+def CorrectFunctionName():
+    ...
+            """,
+            True,
+        ),
+        (
+            """
+def incorrect_function_name():
+    ...
+            """,
+            False,
+        ),
+        (
+            """
+class CorrectClassName:
+    ...
+            """,
+            True,
+        ),
+        (
+            """
+class incorrectClassName:
+    ...
+            """,
+            False,
+        ),
+        (
+            """
+class CorrectClassName:
+    def correct_function_name():
+        ...
+            """,
+            True,
+        ),
+        (
+            """
+class CorrectClassName:
+    def IncorrectFunctionName():
+        ...
+            """,
+            False,
+        ),
+        (
+            """
+def CorrectFunctionName():
+    class CorrectClassName():
+        def correct_function_name():
+            ...
+                      
+class CorrectClassName():
+    def correct_function_name():
+        class CorrectClassName2():
+            ...
+            """,
+            True,
+        ),
+        (
+            """
+async def incorrect_function_name():
+    ...
+            """,
+            False,
+        ),
+        (
+            """
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, 世界")
+}
+            """,
+            False,
+        ),
+    ],
+    ids=[
+        "correct function name, outside of class",
+        "incorrect function name, outside of class",
+        "correct class name",
+        "incorrect class name",
+        "correct function name, inside of class",
+        "incorrect function name, inside of class",
+        "recursive",
+        "async function",
+        "syntax error",
+    ],
+)
+def test_validate(code, expected):
+    assert validate(code) == expected
---
+++ validator/validate.py
@@ -0,0 +1,78 @@
+import ast
+from string import ascii_lowercase, ascii_uppercase
+from typing import Union, Generator
+
+_Node = Union[ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef]
+
+
+def validate_file(filename: str) -> bool:
+    return validate(open(filename, encoding="utf-8").read())
+
+
+def validate(code: str) -> bool:
+    """
+    Checks code syntax and style
+
+    Style guide:
+    1. Name of function outside of class must start with uppercase letter
+    2. Name of function inside of class must start with lowercase letter
+    3. Name of class must start with uppercase letter
+
+    :param code: python code
+    :return: bool if code syntax and style is valid
+    """
+    try:
+        node = ast.parse(code)
+    except SyntaxError:
+        return False
+
+    for child_node in _needs_validation(node):
+        if not _validate_node(child_node, in_class=False):
+            return False
+
+    return True
+
+
+def _validate_node(
+    node: Union[ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef],
+    in_class: bool,
+) -> bool:
+    if _is_function(node):
+        if not _validate_function_name(node.name, in_class):
+            return False
+    else:
+        in_class = True
+
+        if not _validate_class_name(node.name):
+            return False
+
+    for child_node in _needs_validation(node):
+        if not _validate_node(child_node, in_class):
+            return False
+
+    return True
+
+
+def _validate_function_name(name: str, in_class: bool) -> bool:
+    if in_class:
+        return name[0] in ascii_lowercase
+
+    return name[0] in ascii_uppercase
+
+
+def _validate_class_name(name: str) -> bool:
+    return name[0] in ascii_uppercase
+
+
+def _is_function(node: _Node) -> bool:
+    return isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef))
+
+
+def _needs_validation(
+    node: Union[ast.Module, ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef],
+) -> Generator[_Node, None, None]:
+    for child_node in node.body:
+        if isinstance(
+            child_node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)
+        ):
+            yield child_node
